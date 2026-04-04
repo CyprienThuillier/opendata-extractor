7ae0997197c0a67ceb2068ff4d5d76036892f9d5
cyprien <208813283+CyprienThuillier@users.noreply.github.com>
2026-04-05 01:31:43 +0200

    Updating install.sh output

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-pip install -r src/requirements.txt
-playwright install chromium
+echo "[-] Installing dependencies..."
+pip install -r src/requirements.txt > /dev/null 2>&1
+playwright install chromium > /dev/null 2>&1
 echo "[-] Installation completed."
